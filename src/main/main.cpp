0daf51b42ab8fc71ea9868fe56ddfbef49e8978c
CCx3x <acici12138@gmail.com>
2019-04-09 21:54:28 +0800

    main modify

---
+++ src/main/main.cpp
@@ -0,0 +1,21 @@
+// Created by cc on 2019-03-30.
+/********************************************************************************
+ *  File Name       ：   main.cpp
+ *  Founder         ：   cc
+ *  Creation Date   ：   2019-03-30
+ *  Description     ：   leetcode完成题目验证main函数
+ *  Modifier        ：   cc
+ *  Modified Date   ：   2019-04-09
+ *  Version         ：   1.0.0
+ *  CMakeLists(cpp) ：   add_executable(main.cpp, "./main.cpp")
+ ********************************************************************************/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
